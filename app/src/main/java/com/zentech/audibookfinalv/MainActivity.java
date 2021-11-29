09c83651e4b6bb154900baa5d69fd978350c2bd3
RoseZen25 <velezrenz@gmail.com>
2021-11-30 01:16:10 +0800

    login feature initial

--- app/src/main/java/com/zentech/audibookfinalv/MainActivity.java
+++ app/src/main/java/com/zentech/audibookfinalv/MainActivity.java
@@ -1,9 +1,13 @@
 package com.zentech.audibookfinalv;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -13,64 +17,116 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.zentech.audibookfinalv.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private EditText mloginemail, mloginpassword;
+    private RelativeLayout mlogin, mgotosignup;
+    private TextView mgotoforgotpassword;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        //getSupportActionBar().hide();
 
-        setSupportActionBar(binding.toolbar);
+        mloginemail = findViewById(R.id.loginemail);
+        mloginpassword = findViewById(R.id.loginpassword);
+        mlogin = findViewById(R.id.login);
+        mgotoforgotpassword = findViewById(R.id.gotoforgotpassword);
+        mgotosignup = findViewById(R.id.gotosignup);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        /*if(firebaseUser!=null)
+        {
+            finish();
+            startActivity(new Intent(MainActivity.this,HomeActivity.class));
+        }*/
+
+        mgotosignup.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUp.class));
             }
         });
+
+        mgotoforgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ForgotPassword.class));
+            }
+        });
+
+        mlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = mloginemail.getText().toString().trim();
+                String password = mloginpassword.getText().toString().trim();
+
+                if (mail.isEmpty() || password.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"All fields are Required",Toast.LENGTH_SHORT).show();
+                }
+
+                else
+                {
+                    //log in account
+
+                    firebaseAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful())
+                            {
+                                checkmailverification();
+                            }
+
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"Account Does NOT Exist",Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        }
+                    });
+
+                }
+            }
+        });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void checkmailverification()
+    {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(firebaseUser.isEmailVerified())
+        {
+            Toast.makeText(getApplicationContext(),"Logged In",Toast.LENGTH_SHORT).show();
+            finish();
+            startActivity(new Intent(MainActivity.this,HomeActivity.class));
         }
-
-        return super.onOptionsItemSelected(item);
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Verify your Email First!",Toast.LENGTH_SHORT).show();
+            firebaseAuth.signOut();
+        }
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
