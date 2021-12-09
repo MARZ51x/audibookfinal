56bd33a2421160bc9962fe2ff5eda59ea1ed6e9d
acama <acamarbaltz@gmail.com>
2021-12-09 22:36:13 +0800

    Speech-To-Text Implementation (working)

--- app/src/main/java/com/zentech/audibookfinalv/CreateNote.java
+++ app/src/main/java/com/zentech/audibookfinalv/CreateNote.java
@@ -4,14 +4,25 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -22,13 +33,21 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class CreateNote extends AppCompatActivity {
 
     private EditText mcreatetitleofnote, mcreatecontentofnote;
     private ImageButton msavenote, backbttn;
+    private ImageView micBtn;
+    private GifImageView audioGif;
+    public static final Integer RecordAudioRequestCode = 1;
+    private SpeechRecognizer speechRecognizer;
 
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
@@ -48,6 +67,15 @@ public class CreateNote extends AppCompatActivity {
             setTheme(R.style.Theme_Light);
             root.setBackgroundColor(getResources().getColor(android.R.color.white));
         }
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            checkPermission();
+        }
+
+        audioGif = findViewById(R.id.wave);
+        micBtn = findViewById(R.id.imageSpeak);
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        audioGif.setVisibility(View.INVISIBLE);
 
         msavenote = findViewById(R.id.savenote);
         mcreatetitleofnote = findViewById(R.id.createtitleofnote);
@@ -106,11 +134,84 @@ public class CreateNote extends AppCompatActivity {
             }
         });
 
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
 
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
 
+            }
 
+            @Override
+            public void onBeginningOfSpeech() {
+                audioGif.setVisibility(View.VISIBLE);
+                micBtn.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                audioGif.setVisibility(View.INVISIBLE);
+                micBtn.setVisibility(View.VISIBLE);
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                mcreatecontentofnote.append(data.get(0));
+                mcreatecontentofnote.append(". ");
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+        micBtn.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    speechRecognizer.stopListening();
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    speechRecognizer.startListening(speechRecognizerIntent);
+                }
+                return false;
+            }
+        });
 
     }
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
+        }
+    }
+
     public void BackButton(){
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
--- app/src/main/java/com/zentech/audibookfinalv/EditNoteActivity.java
+++ app/src/main/java/com/zentech/audibookfinalv/EditNoteActivity.java
@@ -3,17 +3,28 @@ package com.zentech.audibookfinalv;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -23,10 +34,19 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
+import pl.droidsonroids.gif.GifImageView;
+
 public class EditNoteActivity extends AppCompatActivity {
+
+    public static final Integer RecordAudioRequestCode = 1;
+    private SpeechRecognizer speechRecognizer;
+    private ImageView micBtn;
+    private GifImageView audioGif;
 
     Intent data;
     EditText medittitleofnote, meditcontentofnote;
@@ -36,6 +56,7 @@ public class EditNoteActivity extends AppCompatActivity {
     FirebaseUser firebaseUser;
     FirebaseFirestore firebaseFirestore;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +71,15 @@ public class EditNoteActivity extends AppCompatActivity {
             setTheme(R.style.Theme_Light);
             root.setBackgroundColor(getResources().getColor(android.R.color.white));
         }
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            checkPermission();
+        }
+
+        audioGif = findViewById(R.id.edit_wave);
+        micBtn = findViewById(R.id.edit_imageSpeak);
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        audioGif.setVisibility(View.INVISIBLE);
 
         medittitleofnote = findViewById(R.id.edittitleofnote);
         meditcontentofnote = findViewById(R.id.editcontentofnote);
@@ -109,10 +139,86 @@ public class EditNoteActivity extends AppCompatActivity {
             }
         });
 
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                audioGif.setVisibility(View.VISIBLE);
+                micBtn.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                audioGif.setVisibility(View.INVISIBLE);
+                micBtn.setVisibility(View.VISIBLE);
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                meditcontentofnote.append(data.get(0));
+                meditcontentofnote.append(". ");
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+        micBtn.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    speechRecognizer.stopListening();
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    speechRecognizer.startListening(speechRecognizerIntent);
+                }
+                return false;
+            }
+        });
+
         String notetitle = data.getStringExtra("title");
         String notecontent = data.getStringExtra("content");
         meditcontentofnote.setText(notecontent);
         medittitleofnote.setText(notetitle);
+    }
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
+        }
     }
 
     @Override
