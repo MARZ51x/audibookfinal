4068db2cdd005be33d438864107c087c6d16ad17
ROXANNE <brionesict2@gmail.com>
2021-12-12 23:02:29 +0800

    Krisha pdf..

--- app/src/main/java/com/zentech/audibookfinalv/EditNoteActivity.java
+++ app/src/main/java/com/zentech/audibookfinalv/EditNoteActivity.java
@@ -1,18 +1,23 @@
 package com.zentech.audibookfinalv;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
@@ -33,9 +38,20 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -55,6 +71,13 @@ public class EditNoteActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
     FirebaseFirestore firebaseFirestore;
+
+    ///////////////////for pdf///////////////////////
+    private static final String TAG = "PdfCreatorActivity";
+    private ImageView generatePDF;
+    private File pdfFile;
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 111;
+    ///////////////////for pdf///////////////////////
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -80,6 +103,27 @@ public class EditNoteActivity extends AppCompatActivity {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
             checkPermission();
         }
+
+        //////////////////pdf////////////////////
+        generatePDF = findViewById(R.id.EimagePDF);
+        generatePDF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (medittitleofnote.getText().toString().isEmpty()){
+                    medittitleofnote.setError("Please input title");
+                    medittitleofnote.requestFocus();
+                    return;
+                }
+                try {
+                    createPdfWrapper();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (DocumentException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        /////////////////pdf/////////////////////
 
         audioGif = findViewById(R.id.edit_wave);
         micBtn = findViewById(R.id.edit_imageSpeak);
@@ -239,5 +283,123 @@ public class EditNoteActivity extends AppCompatActivity {
     public void BackButton(){
         onBackPressed();
     }
+
+    //////////////////////pdf/////////////////////
+    private void createPdfWrapper() throws FileNotFoundException,DocumentException{
+        int hasWriteStoragePermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (hasWriteStoragePermission != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!shouldShowRequestPermissionRationale(Manifest.permission.WRITE_CONTACTS)) {
+                    showMessageOKCancel("You need to allow access to Storage",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_ASK_PERMISSIONS);
+                                    }
+                                }
+                            });
+                    return;
+                }
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_ASK_PERMISSIONS);
+            }
+            return;
+        }else {
+            createPdf();
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                    try {
+                        createPdfWrapper();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (DocumentException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    // Permission Denied
+                    Toast.makeText(this, "WRITE_EXTERNAL Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Exit", null)
+                .create()
+                .show();
+    }
+    private void createPdf() throws FileNotFoundException, DocumentException {
+        File docsFolder = new File(Environment.getExternalStorageDirectory() +
+                "/Documents");
+
+        String title=medittitleofnote.getText().toString();
+        if (!docsFolder.exists()) {
+            docsFolder.mkdir();
+            Log.i(TAG, "Create New PDF Folder");
+        }
+
+        Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 22, Font.BOLD
+                | Font.UNDERLINE, BaseColor.BLACK);
+        Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL);
+        Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD, BaseColor.GRAY);
+
+        Paragraph prTitle = new Paragraph();
+        prTitle.setFont(titleFont);
+        prTitle.add(medittitleofnote.getText().toString());
+
+        Paragraph prText = new Paragraph();
+        prText.setFont(smallBold);
+        prText.add(meditcontentofnote.getText().toString());
+
+        int num=0;
+        String save = title+".pdf";
+
+        pdfFile = new File(docsFolder.getAbsolutePath(),save);
+        while(pdfFile.exists()) {
+            save = title+ (num++) +".pdf";
+            pdfFile = new File(docsFolder.getAbsolutePath(), save);
+        }
+        OutputStream output = new FileOutputStream(pdfFile);
+        Document document = new Document();
+        PdfWriter.getInstance(document, output);
+        document.open();
+        //document.add(new Paragraph(inputNoteTitle.getText().toString()));
+        //document.add(new Paragraph(inputNoteSubtitle.getText().toString()));
+        //document.add(new Paragraph(inputNoteText.getText().toString()));
+
+        document.add(prTitle);
+        document.add(new Paragraph("\n\n"));
+        document.add(prText);
+        document.close();
+        previewPdf();
+    }
+    private void previewPdf() {
+        PackageManager packageManager = getPackageManager();
+        Intent testIntent = new Intent(Intent.ACTION_VIEW);
+        testIntent.setType("application/pdf");
+        List list = packageManager.queryIntentActivities(testIntent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        if (list.size() > 0) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            Uri uri = Uri.fromFile(pdfFile);
+            intent.setDataAndType(uri, "application/pdf");
+            startActivity(intent);
+        }else{
+            Toast.makeText(this,"PDF generated",Toast.LENGTH_SHORT).show();
+        }
+    }
+    /////////////////////pdf//////////////////////
 
 }
